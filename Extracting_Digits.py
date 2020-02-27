187e97f490f0d148ab510f777f64947230244136
hast3d <61429878+hast3d@users.noreply.github.com>
2020-02-26 22:09:52 -0500

    Add files via upload

---
+++ Extracting_Digits.py
@@ -0,0 +1,6 @@
+
+#input a number
+num = int(input())
+# output the last digit of the number
+newnum = num%10
+print(newnum)
---
+++ Print_All_Numbers.py
@@ -0,0 +1,7 @@
+
+# Get a number N.
+number_n = int(input())
+
+#Print numbers for 1 to input
+for number in range(number_n):
+    print (number+1, end=" ")
